5eab9abf8a077e5b070ae4e7cb9a21e73ff2b311
Spencer <xspenc3r@gmail.com>
2018-04-13 05:48:41 -0400

    Create Poll View

--- app/src/main/java/com/mohamed/spencer/piepoll/CreatePoll.java
+++ app/src/main/java/com/mohamed/spencer/piepoll/CreatePoll.java
@@ -3,11 +3,11 @@ package com.mohamed.spencer.piepoll;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class CreatePoll extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.create_poll);
     }
 }
