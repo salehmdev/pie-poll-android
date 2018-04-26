583d04e579ea916e60dcb16a6e267223ae53ae43
Spencer <xspenc3r@gmail.com>
2018-04-26 06:06:43 -0400

    TakePoll/PollResults
    TakePoll/PollResults Implemented... Other various fixes

--- app/src/main/java/com/mohamed/spencer/piepoll/TakePollActivity.java
+++ app/src/main/java/com/mohamed/spencer/piepoll/TakePollActivity.java
@@ -1,10 +1,16 @@
 package com.mohamed.spencer.piepoll;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,16 +18,20 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TakePollActivity extends Activity {
+public class TakePollActivity extends Activity implements View.OnClickListener {
 
     TextView textTakePollName;
+    TextView textTakePollCreator;
+    TextView textTakePollMulti;
+    TextView textTakePollAccount;
+    TextView textTakePollIP;
+    Button buttonTakePollVote;
+    Button buttonTakePollResults;
     LinearLayout llTakePoll;
     Poll currentPoll = null;
 
@@ -31,14 +41,26 @@ public class TakePollActivity extends Activity {
         setContentView(R.layout.activity_take_poll);
 
         textTakePollName = (TextView) findViewById(R.id.text_take_poll_name);
+        textTakePollCreator = (TextView) findViewById(R.id.text_take_poll_creator);
+        textTakePollMulti = (TextView) findViewById(R.id.text_take_poll_multi);
+        textTakePollAccount = (TextView) findViewById(R.id.text_take_poll_account);
+        textTakePollIP = (TextView) findViewById(R.id.text_take_poll_ip);
+        buttonTakePollVote = (Button) findViewById(R.id.button_take_poll_vote);
+        buttonTakePollResults = (Button) findViewById(R.id.button_take_poll_results);
         llTakePoll = (LinearLayout) findViewById(R.id.ll_take_poll);
+
+        textTakePollMulti.setVisibility(View.GONE);
+        textTakePollAccount.setVisibility(View.GONE);
+        textTakePollIP.setVisibility(View.GONE);
+
+        buttonTakePollVote.setOnClickListener(this);
+        buttonTakePollResults.setOnClickListener(this);
 
         Bundle bundle = getIntent().getExtras();
         String pollId = bundle.getString("pollid");
-        Toast.makeText(getApplicationContext(), "Taking poll of id: " + pollId, Toast.LENGTH_SHORT).show();
 
-        GetPoll getPoll = new GetPoll();
-        getPoll.execute(pollId);
+        HasVoted hasVoted = new HasVoted(this);
+        hasVoted.execute(pollId);
 
         /*
             Nice resource for adding checkboxes dynamically to a layout:
@@ -46,7 +68,182 @@ public class TakePollActivity extends Activity {
          */
     }
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.button_take_poll_vote:
+                buttonTakePollVote_Clicked();
+                break;
+            case R.id.button_take_poll_results:
+                buttonTakePollResults_Clicked();
+                break;
+        }
+
+        if(view instanceof CheckBox)
+        {
+            if(!currentPoll.multanswer) {
+                for( int i = 0; i<llTakePoll.getChildCount(); i++) {
+                    if(llTakePoll.getChildAt(i) instanceof CheckBox && !view.equals(llTakePoll.getChildAt(i))) {
+                        ((CheckBox) llTakePoll.getChildAt(i)).setChecked(false);
+                    }
+                }
+            }
+            CheckBox clickedCheckBox = (CheckBox) view;
+        }
+    }
+
+    public void buttonTakePollVote_Clicked() {
+        VotePoll votePoll = new VotePoll();
+        ArrayList<String> params = new ArrayList<>();
+        params.add(currentPoll.id);
+        for( int i = 0; i<llTakePoll.getChildCount(); i++) {
+            if(llTakePoll.getChildAt(i) instanceof CheckBox && ((CheckBox) llTakePoll.getChildAt(i)).isChecked()) {
+                params.add(llTakePoll.getChildAt(i).getTag().toString());
+            }
+        }
+
+        if(params.size() < 2) {
+            Toast.makeText(getApplicationContext(), "You must select an option!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String[] paramsArr = params.toArray(new String[params.size()]);
+        votePoll.execute(paramsArr);
+    }
+
+    public void buttonTakePollResults_Clicked() {
+        Intent intent = new Intent(this, ViewPollResultsActivity.class);
+        intent.putExtra("pollid", currentPoll.id);
+        startActivity(intent);
+        finish();
+    }
+
+    private class HasVoted extends AsyncTask<String, Double,  WebRequest.Response> {
+
+        private Context activityContext;
+        public HasVoted(Context c) {
+            this.activityContext = c;
+        }
+
+        public int pollid = 0;
+        protected  WebRequest.Response doInBackground(String... s) {
+            WebRequest.Response r = null;
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("poll", s[0]);
+            pollid = Integer.parseInt(s[0]);
+            WebRequest wr = new WebRequest();
+            try {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String session = prefs.getString("PHPSESSID", "");
+                r = wr.postRequest("http://piepoll.us-east-1.elasticbeanstalk.com/poll/userhasvoted.php", parameters, session);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //publishProgress(progress);
+
+            return r;
+        }
+
+        protected void onProgressUpdate(Double... progress) {
+            //Log.i("a", Double.toString(progress[0]));
+
+        }
+
+        protected void onPreExecute() {
+
+        }
+
+        protected void onPostExecute(WebRequest.Response result) {
+
+            try {
+                JSONObject jsonObject = new JSONObject(result.body);
+
+                int code = jsonObject.getInt("code");
+                if(code != 1) {
+                    Intent intent = new Intent(getApplicationContext(), ViewPollResultsActivity.class);
+                    intent.putExtra("pollid", Integer.toString(pollid));
+                    intent.putExtra("hasvoted", true);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    GetPoll getPoll = new GetPoll(activityContext);
+                    getPoll.execute(Integer.toString(pollid));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("d",  result.body);
+        }
+
+    }
+
+    private class VotePoll extends AsyncTask<String, Double,  WebRequest.Response> {
+
+        protected  WebRequest.Response doInBackground(String... s) {
+            WebRequest.Response r = null;
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("pollid", s[0]);
+            for(int i = 1; i<s.length; i++) {
+                parameters.put("options[" + s[i] + "]", "on");
+            }
+
+            WebRequest wr = new WebRequest();
+            try {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String session = prefs.getString("PHPSESSID", "");
+                r = wr.postRequest("http://piepoll.us-east-1.elasticbeanstalk.com/poll/vote.php", parameters, session);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //publishProgress(progress);
+
+            return r;
+        }
+
+        protected void onProgressUpdate(Double... progress) {
+            //Log.i("a", Double.toString(progress[0]));
+
+        }
+
+        protected void onPostExecute(WebRequest.Response result) {
+            int status = -1;
+            String message = "";
+
+            try {
+                JSONObject jsonObject = new JSONObject(result.body);
+                status = jsonObject.getInt("code");
+                message = jsonObject.getString("error");
+
+                if(status == 1) {
+                    Intent intent = new Intent(getApplicationContext(), ViewPollResultsActivity.class);
+                    intent.putExtra("pollid", currentPoll.id);
+                    intent.putExtra("hasvoted", true);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("d",  result.body);
+        }
+
+    }
+
     private class GetPoll extends AsyncTask<String, Double,  WebRequest.Response> {
+
+        private Context activityContext;
+        public GetPoll(Context c) {
+            this.activityContext = c;
+        }
 
         protected  WebRequest.Response doInBackground(String... s) {
             WebRequest.Response r = null;
@@ -70,6 +267,10 @@ public class TakePollActivity extends Activity {
 
         }
 
+        protected void onPreExecute() {
+
+        }
+
         protected void onPostExecute(WebRequest.Response result) {
 
             try {
@@ -78,9 +279,9 @@ public class TakePollActivity extends Activity {
                 String id = jsonObject.getString("id");
                 String creator = jsonObject.getString("creator");
                 String question = jsonObject.getString("question");
-                Boolean multAnswer = Boolean.parseBoolean(jsonObject.getString("multanswer"));
-                Boolean ipCheck = Boolean.parseBoolean(jsonObject.getString("ipcheck"));
-                Boolean accountReq = Boolean.parseBoolean(jsonObject.getString("accountreq"));
+                Boolean multAnswer = "1".equals(jsonObject.getString("multanswer"));
+                Boolean ipCheck = "1".equals(jsonObject.getString("ipcheck"));
+                Boolean accountReq = "1".equals(jsonObject.getString("accountreq"));
 
                 JSONArray options = jsonObject.getJSONArray("options");
                 ArrayList<PollOption> pollOptions = new ArrayList<>();
@@ -96,16 +297,26 @@ public class TakePollActivity extends Activity {
 
                 textTakePollName.setText(poll.question);
 
-                TextView newTV = new TextView(getApplicationContext());
-                newTV.setText("Creator: " + poll.creator);
-                newTV.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                textTakePollCreator.setText(poll.creator);
 
-                llTakePoll.addView(newTV);
+                if(poll.multanswer) {
+                    textTakePollMulti.setVisibility(View.VISIBLE);
+                }
+
+                if(poll.accountreq) {
+                    textTakePollAccount.setVisibility(View.VISIBLE);
+                }
+
+                if(poll.ipcheck) {
+                    textTakePollIP.setVisibility(View.VISIBLE);
+                }
 
                 for( int i = 0; i<poll.options.size(); i++) {
-                    TextView newTV2 = new TextView(getApplicationContext());
-                    newTV2.setText(poll.options.get(i).description);
-                    llTakePoll.addView(newTV2);
+                    CheckBox chkOption = new CheckBox(getApplicationContext());
+                    chkOption.setText(poll.options.get(i).description);
+                    chkOption.setTag(poll.options.get(i).id);
+                    chkOption.setOnClickListener((TakePollActivity) activityContext);
+                    llTakePoll.addView(chkOption);
                 }
 
 
